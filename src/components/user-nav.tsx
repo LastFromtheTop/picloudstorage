030d3c42619bcf0ddf6f2bb5f07d8fafb93e5a28
vishal sriramoju <vishalsriramoju@gmail.com>
2025-08-02 19:44:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -51,7 +51,7 @@ export default function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt={`@${user.name}`} />
+            <AvatarImage src="" alt={`@${user.name}`} />
             <AvatarFallback>{initials}</AvatarFallback>
           </Avatar>
         </Button>
